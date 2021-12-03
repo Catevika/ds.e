740bdfb00681f9f5ae5e24c73591e59d3cd2efbf
Catevika <dominique.bello@outlook.fr>
2021-12-03 18:05:30 +0100

    Amend rollup.config, Color index, index.html & clean up ignored files

--- playgrounds/react/src/index.tsx
+++ playgrounds/react/src/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { Button } from "@ds.e/react/src";
+import { /* Button, */ Color } from "@ds.e/react/src";
 
-import "@ds.e/scss/lib/Button.css";
+// import "@ds.e/scss/lib/Button.css";
+import "@ds.e/scss/lib/Utilities.css";
 
 ReactDOM.render(
-	<Button label='Example Button' />,
+	<>
+		{/* <Button label='Example Button' /> */}
+		<Color hexCode='#000' width='xl' height='xl' />
+	</>,
 	document.querySelector("#root")
 );
