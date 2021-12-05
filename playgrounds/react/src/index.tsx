88962c11652f548dd755a8f439a7453bc96adce5
Catevika <dominique.bello@outlook.fr>
2021-12-05 18:09:48 +0100

    Add Select molecule without style

--- playgrounds/react/src/index.tsx
+++ playgrounds/react/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { /* Button, Color, */ Text, Margin } from "@ds.e/react/src";
+import { /* Button, Color, Text, Margin, */ Select } from "@ds.e/react/src";
 
 import "@ds.e/scss/lib/Global.css";
 import "@ds.e/scss/lib/Utilities.css";
@@ -9,13 +9,20 @@ import "@ds.e/scss/lib/Button.css";
 import "@ds.e/scss/lib/Text.css";
 import "@ds.e/scss/lib/Margin.css";
 
+const options = [
+	{ label: "Strict Black", value: "strict-black" },
+	{ label: "Heavenly Green", value: "heavenly-green" },
+	{ label: "Sweet Pink", value: "pink" }
+];
+
 ReactDOM.render(
 	<>
 		{/* <Button hexCode='#000' />
-		<Color width='xl' height='xl' /> */}
+		<Color width='xl' height='xl' />
 		<Margin left space='none'>
 			<Text size='xl'>This is some text</Text>
-		</Margin>
+		</Margin> */}
+		<Select options={options} />
 	</>,
 	document.querySelector("#root")
 );
