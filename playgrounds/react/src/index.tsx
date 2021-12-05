f33e0ff1de61d77920ec35256fd5ca5e9d7adf83
Catevika <dominique.bello@outlook.fr>
2021-12-05 17:06:36 +0100

    Add Margin, amend Spacing  & Text

--- playgrounds/react/src/index.tsx
+++ playgrounds/react/src/index.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { /* Button, */ Color } from "@ds.e/react/src";
+import { /* Button, Color, */ Text, Margin } from "@ds.e/react/src";
 
-// import "@ds.e/scss/lib/Button.css";
+import "@ds.e/scss/lib/Global.css";
 import "@ds.e/scss/lib/Utilities.css";
+import "@ds.e/scss/lib/Button.css";
+import "@ds.e/scss/lib/Text.css";
+import "@ds.e/scss/lib/Margin.css";
 
 ReactDOM.render(
 	<>
-		{/* <Button label='Example Button' /> */}
-		<Color hexCode='#000' width='xl' height='xl' />
+		{/* <Button hexCode='#000' />
+		<Color width='xl' height='xl' /> */}
+		<Margin left space='none'>
+			<Text size='xl'>This is some text</Text>
+		</Margin>
 	</>,
 	document.querySelector("#root")
 );
